293a2a43def2c13f7f44fd7bf5981eeacb7c7fbd
D.D.M. <dimaddm18@gmail.com>
2025-06-02 12:17:13 +0300

    Update layout.tsx
    Yandex, Google pixels

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -42,6 +42,33 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WYRS6B59PE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-WYRS6B59PE');
+        </script>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();
+           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+           ym(102303375, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+           });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/102303375" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+      </head>
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
