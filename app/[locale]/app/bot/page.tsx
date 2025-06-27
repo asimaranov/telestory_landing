9641fd4de4670f56b4e61be664c70eb4d606b50f
D.D.M. <dimaddm18@gmail.com>
2025-06-27 15:27:30 +0300

    Update page.tsx

--- app/[locale]/app/bot/page.tsx
+++ app/[locale]/app/bot/page.tsx
@@ -1,10 +1,4 @@
 import { redirect } from "next/navigation";
 
-export default async function Start({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const startParam = searchParams.start || "landing";
-  redirect(`https://t.me/tele_story_bot?start=${startParam}`);
-}
+export default async function Start() {Add commentMore actions
+  redirect("https://t.me/tele_story_bot?start=landing");
