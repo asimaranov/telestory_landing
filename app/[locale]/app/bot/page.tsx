d5f0d5f4feaeb88e6321f894ccc6650f76091007
asimaranov <a.simaranov@gmail.com>
2025-06-27 14:09:22 +0300

    Added start forwardong

--- app/[locale]/app/bot/page.tsx
+++ app/[locale]/app/bot/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
-export default async function Start() {
-  redirect("https://t.me/tele_story_bot?start=landing");
+export default async function Start({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const startParam = searchParams.start || "landing";
+  redirect(`https://t.me/tele_story_bot?start=${startParam}`);
 }
